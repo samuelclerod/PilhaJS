118286e6922b3303f35107ffec6840391a6e74c6
Samuel Rodrigues <samuelclerod@gmail.com>
2018-03-02 15:13:04 -0300

    Ajustes de legibilidade

--- js/stack.js
+++ js/stack.js
@@ -2,13 +2,13 @@ function Stack() {
     var items = [];
     this.push = function(element) {
         items.push(element);
-    };
+    }
     this.pop = function() {
         return (items.pop());
-    };
+    }
     this.peek = function() {
         return items[items.length - 1];
-    };
+    }
     this.isEmpty = function() {
         return items.length == 0;
     }
