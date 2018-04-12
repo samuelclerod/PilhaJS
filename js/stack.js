c6c0b8406581cf66b097fb459d6001d18bf9056e
samuelclerod <samuelclerod@gmail.com>
2018-04-12 17:20:01 -0300

    refatorado para ES6

--- js/stack.js
+++ js/stack.js
@@ -1,29 +1,13 @@
-function Stack() {
-    var items = [];
-    this.push = function(element) {
-        items.push(element);
+class Stack {
+    
+    constructor(){
     }
-    this.pop = function() {
-        return (items.pop());
-    }
-    this.peek = function() {
-        return items[items.length - 1];
-    }
-    this.isEmpty = function() {
-        return items.length == 0;
-    }
-    this.size = function() {
-        return items.length;
-    }
-    this.clear = function() {
-        items = []
-    }
-    this.print = function(separator) {
-        if (!separator) separator = ''
-        var s = ''
-        for (var i = items.length - 1; i >= 0; i--) {
-            s += items[i] + " " + separator;
-        }
-        return s;
-    }
+    
+    push (element) {}
+    pop() {}
+    peek() {}
+    isEmpty() {}
+    size() {}
+    clear() {}
+    print(separator=', ') {}
 }
