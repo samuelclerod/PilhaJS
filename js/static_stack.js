540f0f4c8de98945eb4a8298d0392dc9cebac6b9
samuelclerod <samuelclerod@gmail.com>
2018-09-26 21:49:28 -0300

    iniciada a implementação da pilha estatica

--- js/static_stack.js
+++ js/static_stack.js
@@ -1,26 +1,35 @@
 class StaticStack {
 
     constructor(){
+        this.data = [];
     }
     
     push (element) {
+        this.data.push(element);
     }
 
     pop() {
+        return this.data.pop();
     }
 
     peek() {
+        return this.data.length-1;
     }
 
     isEmpty() {
+        return this.data.size()===0
     }
 
     size() {
+        return this.data.length;
     }
 
     clear() {
+        this.data = [];
     }
     
     print(separator=' - ') {
+        
     }
+
 }
