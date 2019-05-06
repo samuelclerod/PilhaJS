069e460a926d315b81699d8c5c183eb3b9317a07
samuelclerod <samuelclerod@gmail.com>
2019-05-06 19:42:34 -0300

    Implementada a pilha dinâmica

---
+++ js/dinamic_stack.js
@@ -0,0 +1,51 @@
+class DinamicStack {
+
+    constructor() {
+        this.top = null;
+        this.length = 0;
+    }
+
+    push(element) {
+        let node  = new Node(element);
+        if (!this.isEmpty()) {
+            node.next = this.top;
+        }
+        this.top = node;
+        this.length++;
+    }
+
+    pop() {
+        if(this.isEmpty()) 
+            return null;
+        let diedNode = this.top;
+        this.top = this.top.next;
+        diedNode.next = null;
+        this.length--;
+        return diedNode.content;
+    }
+
+    peek() {
+        return this.length-1;
+    }
+
+    isEmpty() {
+        return this.top===null;
+    }
+
+    size() {
+        return this.length;
+    }
+
+    clear() {
+        this.top=null;
+    }
+
+    print(separator = ' - ') {
+        let text = "", current = this.top;
+        while (current!==null) {
+            text += current.content + separator;
+            current = current.next
+        }
+        return text.substr(0, text.length - separator.length);
+    }
+}
